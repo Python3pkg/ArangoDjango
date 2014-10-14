1eaee66b52eb3b3a453c415edfc23e23588fec7a
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-10-14 19:34:30 +0200

    Starting to program

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ArangoDjango',
+    version= '0.0.1',
+    packages=find_packages(),
+    requires=[
+        'Django',
+        'djangorestframework',
+        'ArangoPy',
+    ],
+    url='https://github.com/saeschdivara/ArangoDjango',
+    license='MIT',
+    author='saskyrardisaskyr',
+    author_email='saeschdivara@gmail.com',
+    description='Bridge between the django and the arangodb world'
+)
