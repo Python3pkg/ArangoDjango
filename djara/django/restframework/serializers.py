f75bd69ae9b108cec2c747205bbe36e43d393a86
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-10-18 18:03:55 +0200

    Starting with the restframework for django

---
+++ djara/django/restframework/serializers.py
@@ -0,0 +1,90 @@
+from rest_framework import serializers, fields, relations
+from arangodb.orm.fields import NumberField, CharField, ForeignKeyField, ManyToManyField
+
+
+class CollectionModelSerializerOptions(serializers.SerializerOptions):
+    """
+    Meta class options for CollectionModelSerializer
+    """
+    def __init__(self, meta):
+        super(CollectionModelSerializerOptions, self).__init__(meta)
+        self.model = getattr(meta, 'model', None)
+        self.read_only_fields = getattr(meta, 'read_only_fields', ())
+        self.write_only_fields = getattr(meta, 'write_only_fields', ())
+
+class CollectionModelSerializer(serializers.Serializer):
+    """
+    """
+
+    _options_class = CollectionModelSerializerOptions
+
+    field_mapping = {
+        NumberField: fields.IntegerField,
+        CharField: fields.CharField,
+    }
+
+    def get_fields(self):
+        """
+        """
+
+        return_fields = {}
+
+        model_class = self.opts.model
+
+        model_class_fields = model_class.get_collection_fields_dict()
+
+        for field_name in model_class_fields:
+            field = model_class_fields[field_name]
+
+            kwargs = {}
+
+            if field.__class__ in self.field_mapping:
+                restframework_field_class = self.field_mapping[field.__class__]
+                restframework_field = restframework_field_class(**kwargs)
+
+                return_fields[field_name] = restframework_field
+
+            # get_related_field
+            if isinstance(field, ForeignKeyField):
+                return_fields[field_name] = self.get_related_field(model_field=field, related_model=field.relation_class, to_many=False)
+            if isinstance(field, ManyToManyField):
+                return_fields[field_name] = self.get_related_field(model_field=field, related_model=field.relation_class, to_many=True)
+
+        return return_fields
+
+    def get_field(self, model_field):
+        """
+        Creates a default instance of a basic non-relational field.
+        """
+
+    def get_related_field(self, model_field, related_model, to_many):
+        """
+        Creates a default instance of a flat relational field.
+
+        Note that model_field will be `None` for reverse relationships.
+        """
+        # TODO: filter queryset using:
+        # .using(db).complex_filter(self.rel.limit_choices_to)
+
+        kwargs = {
+            'queryset': related_model.objects,
+            'many': to_many
+        }
+
+        if model_field:
+            kwargs['required'] = not(model_field.null or model_field.blank)
+            # if model_field.help_text is not None:
+            #     kwargs['help_text'] = model_field.help_text
+            # if model_field.verbose_name is not None:
+            #     kwargs['label'] = model_field.verbose_name
+
+            # if not model_field.editable:
+            #     kwargs['read_only'] = True
+
+            # if model_field.verbose_name is not None:
+            #     kwargs['label'] = model_field.verbose_name
+
+            # if model_field.help_text is not None:
+            #     kwargs['help_text'] = model_field.help_text
+
+        return relations.PrimaryKeyRelatedField(**kwargs)
