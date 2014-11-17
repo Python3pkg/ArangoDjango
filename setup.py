04b62bb12ecdd026661b32db1582524a87307835
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-11-17 12:00:22 +0100

    Added quite some functionality

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ArangoDjango',
-    version= '0.0.9',
+    version= '0.0.10',
     packages=find_packages(),
     requires=[
         'Django',
