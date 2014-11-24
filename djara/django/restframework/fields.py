26eb5095b1dddcea03f9aaa927e23989f9b0a308
Sascha Häusler <sascha.haeusler@netzbarkeit.ch>
2014-11-24 07:26:24 +0100

    For now this will give only the media url to the file and not the full path on the server

--- djara/django/restframework/fields.py
+++ djara/django/restframework/fields.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+
 from rest_framework.fields import FileField
 
 
 class RestFileField(FileField):
 
     def to_native(self, value):
-        return value
+        return '%s%s' % (settings.MEDIA_URL,  value.split(settings.MEDIA_URL)[1])
