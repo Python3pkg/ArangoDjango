6dfd816ecdc997bed796d811d7a76bfd290647d9
Sascha <saskyrar@Saschas-MacBook-Pro.local>
2014-12-01 16:39:30 +0100

    This should fix the form problem for the admin

--- djara/django/contrib/admin/options.py
+++ djara/django/contrib/admin/options.py
@@ -1,4 +1,9 @@
+from django import forms
 from django.contrib.admin.options import ModelAdmin
+from django.contrib.admin.util import flatten_fieldsets
+from django.core.exceptions import FieldError
+from django.forms.models import modelform_defines_fields, modelform_factory
+from djara.django.forms.forms import CollectionForm
 
 
 class CollectionAdmin(ModelAdmin):
@@ -6,3 +11,44 @@ class CollectionAdmin(ModelAdmin):
     def __init__(self, model, admin_site):
 
         super(CollectionAdmin, self).__init__(model, admin_site)
+
+
+    form = CollectionForm
+
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Returns a Form class for use in the admin add view. This is used by
+        add_view and change_view.
+        """
+        if 'fields' in kwargs:
+            fields = kwargs.pop('fields')
+        else:
+            fields = flatten_fieldsets(self.get_fieldsets(request, obj))
+        if self.exclude is None:
+            exclude = []
+        else:
+            exclude = list(self.exclude)
+        exclude.extend(self.get_readonly_fields(request, obj))
+        if self.exclude is None and hasattr(self.form, '_meta') and self.form._meta.exclude:
+            # Take the custom ModelForm's Meta.exclude into account only if the
+            # ModelAdmin doesn't define its own.
+            exclude.extend(self.form._meta.exclude)
+        # if exclude is an empty list we pass None to be consistent with the
+        # default on modelform_factory
+        exclude = exclude or None
+        defaults = {
+            "form": self.form,
+            "fields": fields,
+            "exclude": exclude,
+        }
+        defaults.update(kwargs)
+
+        if defaults['fields'] is None and not modelform_defines_fields(defaults['form']):
+            defaults['fields'] = forms.ALL_FIELDS
+
+        try:
+            return modelform_factory(self.model, **defaults)
+        except FieldError as e:
+            raise FieldError('%s. Check fields/fieldsets/exclude attributes of class %s.'
+                             % (e, self.__class__.__name__))
