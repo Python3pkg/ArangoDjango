047c5945a38d8a3e9524314a1b68f2996b3d407c
Sascha <saskyrar@Saschas-MacBook-Pro.local>
2014-12-01 16:11:28 +0100

    Updated admin urls

--- djara/django/contrib/admin/urls.py
+++ djara/django/contrib/admin/urls.py
@@ -3,11 +3,15 @@
 # It is also provided as a convenience to those who want to deploy these URLs
 # elsewhere.
 
-from django.conf.urls import patterns, url
+from django.contrib import admin
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('djara.django.contrib.admin.views',
+
     url(r'^login/$', 'login', name='login'),
     url(r'^logout/$', 'logout', name='logout'),
+    url(r'^(?P<app_label>\w+)/$', admin.site.index, name='app_list'),
+    url(r'^', include(admin.site.urls)),
     # url(r'^password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
     # url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
     # url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
