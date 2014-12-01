94bae49b393a7493f223df5aa5a204e5a5c57192
Sascha <saskyrar@Saschas-MacBook-Pro.local>
2014-12-01 16:12:19 +0100

    We need to change url reversion

--- djara/django/contrib/admin/sites.py
+++ djara/django/contrib/admin/sites.py
@@ -1,5 +1,12 @@
 import django
 from django.contrib.admin.sites import AdminSite as DjangoAdminSite
+from django.core.urlresolvers import reverse, NoReverseMatch
+from django.http.response import Http404
+from django.template.defaultfilters import capfirst
+from django.template.response import TemplateResponse
+from django.views.decorators.cache import never_cache
+from django.utils.translation import ugettext as _
+import six
 
 
 class CollectionAdminSite(DjangoAdminSite):
@@ -49,6 +56,126 @@ class CollectionAdminSite(DjangoAdminSite):
         #
         #         # Instantiate the admin class to save in the registry
             self._registry[model] = admin_class(model, self)
+
+    @never_cache
+    def index(self, request, extra_context=None):
+        """
+        Displays the main admin index page, which lists all of the installed
+        apps that have been registered in this site.
+        """
+        app_dict = {}
+        user = request.user
+        for model, model_admin in self._registry.items():
+            app_label = model._meta.app_label
+            has_module_perms = user.has_module_perms(app_label)
+
+            if has_module_perms:
+                perms = model_admin.get_model_perms(request)
+
+                # Check whether user has any perm for this module.
+                # If so, add the module to the model_list.
+                if True in perms.values():
+                    info = (app_label, model._meta.model_name)
+                    model_dict = {
+                        'name': capfirst(model._meta.verbose_name_plural),
+                        'object_name': model._meta.object_name,
+                        'perms': perms,
+                    }
+                    if perms.get('change', False):
+                        try:
+                            model_dict['admin_url'] = reverse('admin:admin:%s_%s_changelist' % info, current_app=self.name)
+                        except NoReverseMatch:
+                            pass
+                    if perms.get('add', False):
+                        try:
+                            model_dict['add_url'] = reverse('admin:admin:%s_%s_add' % info, current_app=self.name)
+                        except NoReverseMatch:
+                            pass
+                    if app_label in app_dict:
+                        print(model_dict)
+                        app_dict[app_label]['models'].append(model_dict)
+                    else:
+                        app_dict[app_label] = {
+                            'name': app_label.title(),
+                            'app_label': app_label,
+                            'app_url': reverse('admin:app_list', kwargs={'app_label': app_label}, current_app=self.name),
+                            'has_module_perms': has_module_perms,
+                            'models': [model_dict],
+                        }
+
+        # Sort the apps alphabetically.
+        app_list = list(six.itervalues(app_dict))
+        app_list.sort(key=lambda x: x['name'])
+
+        # Sort the models alphabetically within each app.
+        for app in app_list:
+            app['models'].sort(key=lambda x: x['name'])
+
+        context = {
+            'title': _('Site administration'),
+            'app_list': app_list,
+        }
+        context.update(extra_context or {})
+        return TemplateResponse(request, self.index_template or
+                                'admin/index.html', context,
+                                current_app=self.name)
+
+
+    def app_index(self, request, app_label, extra_context=None):
+        user = request.user
+        has_module_perms = user.has_module_perms(app_label)
+        app_dict = {}
+        for model, model_admin in self._registry.items():
+            if app_label == model._meta.app_label:
+                if has_module_perms:
+                    perms = model_admin.get_model_perms(request)
+
+                    # Check whether user has any perm for this module.
+                    # If so, add the module to the model_list.
+                    if True in perms.values():
+                        info = (app_label, model._meta.model_name)
+                        model_dict = {
+                            'name': capfirst(model._meta.verbose_name_plural),
+                            'object_name': model._meta.object_name,
+                            'perms': perms,
+                        }
+                        if perms.get('change', False):
+                            try:
+                                model_dict['admin_url'] = reverse('admin:admin:%s_%s_changelist' % info, current_app=self.name)
+                            except NoReverseMatch:
+                                pass
+                        if perms.get('add', False):
+                            try:
+                                model_dict['add_url'] = reverse('admin:admin:%s_%s_add' % info, current_app=self.name)
+                            except NoReverseMatch:
+                                pass
+                        if app_dict:
+                            app_dict['models'].append(model_dict),
+                        else:
+                            # First time around, now that we know there's
+                            # something to display, add in the necessary meta
+                            # information.
+                            app_dict = {
+                                'name': app_label.title(),
+                                'app_label': app_label,
+                                'app_url': '',
+                                'has_module_perms': has_module_perms,
+                                'models': [model_dict],
+                            }
+        if not app_dict:
+            raise Http404('The requested admin page does not exist.')
+        # Sort the models alphabetically within each app.
+        app_dict['models'].sort(key=lambda x: x['name'])
+        context = {
+            'title': _('%s administration') % capfirst(app_label),
+            'app_list': [app_dict],
+        }
+        context.update(extra_context or {})
+
+        return TemplateResponse(request, self.app_index_template or [
+            'admin/%s/app_index.html' % app_label,
+            'admin/app_index.html'
+        ], context, current_app=self.name)
 
 
 django.contrib.admin.site = CollectionAdminSite()
