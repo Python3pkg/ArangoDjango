d5830797394b66c3d7c742a2d211b800357a0c11
Sascha <saskyrar@Saschas-MacBook-Pro.local>
2014-12-02 21:08:22 +0100

    Added layer for Django

--- djara/django/models/model.py
+++ djara/django/models/model.py
@@ -2,4 +2,20 @@ from arangodb.orm.models import CollectionModel
 
 
 class DjangoModel(CollectionModel):
-    pass
+
+    @classmethod
+    def extend_meta_data(cls, model_class, class_meta):
+        """
+        """
+
+        def get_field_by_name(field_name):
+            """
+            """
+
+            fields = model_class.get_all_fields()
+            if field_name in fields:
+                return [fields[field_name]]
+            else:
+                return []
+
+        class_meta.get_field_by_name = get_field_by_name
