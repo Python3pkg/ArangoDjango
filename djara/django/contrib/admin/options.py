ead13892c11918542aac73b08ed3decdca028741
Sascha <saskyrar@Saschas-MacBook-Pro.local>
2014-12-02 17:31:30 +0100

    Queryset is no rightly retrieved

--- djara/django/contrib/admin/options.py
+++ djara/django/contrib/admin/options.py
@@ -107,3 +107,16 @@ class CollectionAdmin(ModelAdmin):
             "admin/%s/change_form.html" % app_label,
             "admin/change_form.html"
         ], context, current_app=self.admin_site.name)
+
+
+    def get_queryset(self, request):
+        """
+        Returns a QuerySet of all model instances that can be edited by the
+        admin site. This is used by changelist_view.
+        """
+        qs = self.model.objects.all()
+        # TODO: this should be handled by some parameter to the ChangeList.
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
