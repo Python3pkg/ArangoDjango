c9698a9e8a399d0efc3c88cb6a797bf4d5d79862
Sascha <saskyrar@Saschas-MacBook-Pro.local>
2014-12-08 09:34:56 +0100

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ArangoDjango',
-    version= '0.0.17',
+    version= '0.0.18',
     packages=find_packages(),
     include_package_data=True,
     requires=[
