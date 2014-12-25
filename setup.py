a8171ff7475d3d6f562e1a90859b836b732cf318
Sascha <sascha.haeusler@netzbarkeit.ch>
2014-12-25 14:53:56 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ArangoDjango',
-    version= '0.0.20',
+    version= '0.0.21',
     packages=find_packages(),
     include_package_data=True,
     requires=[
